32bd5764cae8f3d2f21acc71270a994fbc44f2d7
Luis-Nav <luiscatodd@gmail.com>
2024-05-07 21:07:43 -0600

    Add authRequiredMiddleware to carrer routes

--- backend/src/routes/carrer.routes.ts
+++ backend/src/routes/carrer.routes.ts
@@ -3,16 +3,16 @@ import { Router } from "express";
 // Controllers
 import { addCarrer, deleteCarrer, getCarrer, getCarrers, updateCarrer } from "../controllers";
 // Middlewares
-import { schemaValidation } from "../middlewares";
+import { schemaValidation, authRequired } from "../middlewares";
 // Schemas
 import { carrerSchema } from "../schemas";
 
 const router = Router();
 
-router.get("/", getCarrers);
-router.get("/:code", getCarrer);
-router.post("/", schemaValidation(carrerSchema), addCarrer);
-router.put("/:oldCode", updateCarrer);
-router.delete("/:code", deleteCarrer);
+router.get("/", authRequired, getCarrers);
+router.get("/:code", authRequired, getCarrer);
+router.post("/", authRequired, schemaValidation(carrerSchema), addCarrer);
+router.put("/:oldCode", authRequired, updateCarrer);
+router.delete("/:code", authRequired, deleteCarrer);
 
 export default router;
