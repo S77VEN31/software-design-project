f90f93f4cfa94bfb2182a9a97373a2b5457dfd78
Luis-Nav <luiscatodd@gmail.com>
2024-05-10 20:48:41 -0600

    Add implementation of comment routes for activities and schedules

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,5 +2,6 @@ import activityRoutes from "./activity.routes";
 import authenticationRoutes from "./authentication.routes";
 import careerRoutes from "./career.routes";
 import userRoutes from "./user.routes";
+import commentRoutes from "./comment.routes";
 
-export { activityRoutes, authenticationRoutes, careerRoutes, userRoutes };
+export { activityRoutes, authenticationRoutes, careerRoutes, userRoutes, commentRoutes };
