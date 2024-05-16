2e6088fed77edc05abe81f3e8fda6a870292acb7
S77VEN <stivensg313131@outlook.com>
2024-05-16 04:41:08 -0700

    Update static file serving path for frontend build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 import app from "./app"; // Importa la configuración de tu aplicación Express
 import { connect } from "./database"; // Conecta a la base de datos
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Define el puerto
 const port = process.env.PORT || 3000;
 
+// __dirname y __filename para ES Modules
+const __dirname = path.resolve();
+
 // Middleware para servir archivos estáticos de la carpeta dist
-const distPath = path.join(__dirname, "..", "frontend", "dist");
+const distPath = path.join(__dirname, "frontend", "dist");
 app.use(express.static(distPath));
 
 // Ruta para servir el archivo HTML de la build de React
